79ecbf61074d8f3c92bfc5b63e36d348ffbe0602
Wang HoEun <hoeunwang@gmail.com>
2024-11-16 20:48:25 +0900

    chore: project settings

---
+++ src/main/java/Iot/org/tobePJ/TobePjApplication.java
@@ -0,0 +1,13 @@
+package Iot.org.tobePJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobePjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobePjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Iot/org/tobePJ/TobePjApplicationTests.java
@@ -0,0 +1,13 @@
+package Iot.org.tobePJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobePjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
